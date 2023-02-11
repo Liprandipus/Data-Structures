50bf63f9658e4a6a366380d2f989f3c6d7bc741e
Lyprandos Liatsos <lyprandosliatsos03@gmail.com>
2023-02-11 16:03:31 +0200

    Otinanai

---
+++ quicksort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void quicksort(int *array, int bot, int top);
+void showarray(int *array, int s, int f);
+
+
+int main(){
+    
+    int array[] = {5,25,1,4,19,5,44};
+    int n = sizeof(array)/sizeof(int); // Gives back the number of elements of the array
+
+
+
+    return 0;
+}
